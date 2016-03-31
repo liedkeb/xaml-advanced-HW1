2a0f21ee67cf700a6d90498c1b2d1273ac4eaf1a
Bartosz Liedke <liedkeb@gmail.com>
2016-03-31 19:21:53 +0200

    adding missing ViewModels

---
+++ RestaurantMenager.ViewModel/ExpediteViewModel.cs
@@ -0,0 +1,50 @@
+﻿using RestaurantManager.Models;
+using RestaurantMenager.ViewModel;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+
+namespace RestaurantManager.ViewModels
+{
+    public class ExpediteViewModel : ViewModel
+    {
+        public ExpediteViewModel()
+        {
+            DeleteOrderCommand = new DelegateCommand<Order>(DeleteOrder);
+            DeleteAllOrdersCommand = new DelegateCommand<object>(DeleteAllOrders);
+        }
+        protected override void OnDataLoaded()
+        {
+            OrderItems = new ObservableCollection<Order>(base.Repository.Orders);
+        }
+        private ObservableCollection<Order> _orderItems;
+        public ObservableCollection<Order> OrderItems
+        {
+            get { return _orderItems; }
+            set
+            {
+                if (_orderItems != value)
+                {
+                    this._orderItems = value;
+                    this.NotifyPropertyChanged();
+
+                }
+            }
+        }
+
+        public ICommand DeleteOrderCommand { get; private set; }
+        private async void DeleteOrder(Order parameter)
+        {
+            RestaurantContext repo = await RestaurantContextFactory.GetRestaurantContextAsync();
+            repo.Orders.Remove(parameter);
+            OnDataLoaded();
+        }
+
+        public ICommand DeleteAllOrdersCommand { get; private set; }
+        private async void DeleteAllOrders(object parameter)
+        {
+            RestaurantContext repo = await RestaurantContextFactory.GetRestaurantContextAsync();
+            repo.Orders.Clear();
+            OnDataLoaded();
+        }
+    }
+}
---
+++ RestaurantMenager.ViewModel/OrderViewModel.cs
@@ -0,0 +1,95 @@
+﻿using RestaurantManager.Models;
+using RestaurantMenager.ViewModel;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using System.Linq;
+
+namespace RestaurantManager.ViewModels
+{
+    public class OrderViewModel : ViewModel
+    {       
+        public OrderViewModel()
+        {
+            this.CurrentlySelectedMenuItems = new ObservableCollection<MenuItem>();
+            AddToOrderCommand = new DelegateCommand<object>(AddToOrder);
+            SubmitOrderCommand = new DelegateCommand<object>(SubmitOrder);
+        }
+        protected override void OnDataLoaded()
+        {
+            this.MenuItems = base.Repository.StandardMenuItems;
+
+        }
+
+        private List<MenuItem> _menuItems = null;
+        public List<MenuItem> MenuItems
+        {
+            get
+            {
+                return this._menuItems;
+            }
+            set
+            {
+                if (this._menuItems!=value)
+                {
+                    this._menuItems = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+        
+        public ObservableCollection<MenuItem> CurrentlySelectedMenuItems { get; private set; }
+        private MenuItem _selectedMenuItem;
+        public MenuItem SelectedMenuItem
+        {
+            get { return _selectedMenuItem; }
+            set
+            {
+                _selectedMenuItem = value;
+                NotifyPropertyChanged();
+            }
+        }
+        private string _specialRequests;
+        public string SpecialRequests
+        {
+            get { return _specialRequests; }
+            set
+            {
+                if (_specialRequests != value)
+                {
+                    _specialRequests = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public ICommand AddToOrderCommand { get; private set; }
+        public ICommand SubmitOrderCommand { get; private set; }
+
+        private void AddToOrder (object parameter)
+        {
+            if (SelectedMenuItem!=null)
+                this.CurrentlySelectedMenuItems.Add(this.SelectedMenuItem);
+        }
+        private async void SubmitOrder(object parameter)
+        {
+            if (CurrentlySelectedMenuItems.Count > 0)
+            {
+                RestaurantContext repo = await RestaurantContextFactory.GetRestaurantContextAsync();
+                repo.Orders.Add(new Order
+                {
+                    Items = CurrentlySelectedMenuItems.ToList(),
+                    Complete = false,
+                    Id = repo.Orders.Count,
+                    Table = repo.Tables.First(),
+                    Expedite = false,
+                    SpecialRequests = this.SpecialRequests
+
+                });
+                CurrentlySelectedMenuItems.Clear();
+                SpecialRequests = "";
+                new Windows.UI.Popups.MessageDialog("Order has been submitted!").ShowAsync();
+            }
+        }
+    }
+}
